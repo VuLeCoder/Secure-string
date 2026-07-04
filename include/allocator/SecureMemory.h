6f03477c7cf55d561758daca20033f57ba34ead2
VuzLe <tle63917@gmail.com>
2026-07-04 21:03:05 +0700

    feat(memory): add secure_compare function for secure memory comparison

--- include/allocator/SecureMemory.h
+++ include/allocator/SecureMemory.h
@@ -5,4 +5,6 @@ namespace secure {
     void secure_memzero(void* ptr, std::size_t size);
     bool lock_memory(void* ptr, std::size_t size);
     bool unlock_memory(void* ptr, std::size_t size);
+
+    bool secure_compare(const void* ptr1, const void* ptr2, std::size_t size);
 };
--- src/allocator/SecureMemory.cpp
+++ src/allocator/SecureMemory.cpp
@@ -1,5 +1,6 @@
 #include "./../../include/allocator/SecureMemory.h"
 
+#include <cstddef>
 #include <cstring>
 #include <memoryapi.h>
 #include <minwindef.h>
@@ -15,4 +16,15 @@ bool secure::lock_memory(void *ptr, std::size_t size) {
 
 bool secure::unlock_memory(void *ptr, std::size_t size) {
     return VirtualUnlock(ptr, size) != FALSE;
+}
+
+bool secure::secure_compare(const void *ptr1, const void *ptr2, std::size_t size) {
+    const auto* a = static_cast<const unsigned char*>(ptr1);
+    const auto* b = static_cast<const unsigned char*>(ptr2);
+    
+    unsigned char diff = 0;
+    for(std::size_t i=0; i<size; ++i) {
+        diff |= a[i] ^ b[i];
+    }
+    return diff == 0;
 }
